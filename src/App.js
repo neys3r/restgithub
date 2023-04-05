2e23ea9c806cf8a715d225237f8e3cb866947596
Neyser QSM <neyser.nht@gmail.com>
2023-04-05 16:39:50 +0200

    Inicio Repo API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [keyword, setKeybord] = useState("");
+
+  const fetchData = () => {
+    // Llamada al API REST
+    const url = `https://api.github.com/search/repositories?q=${keyword}`;
+    fetch(url)
+    .then((response)=> response.json())
+    .then((responseData) => {
+      setData(responseData.items);
+    })
+
+  }
+  const handleChange = (e) => {
+    setKeybord(e.target.value);
+  }
+
+  const tableRows = data.map((item, index)=> 
+    <tr key={index}>
+      <td>{item.full_name}</td>
+      <td><a href={item.html_url}>{item.html_url}</a></td>
+      </tr>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={handleChange}/>
+      <button onClick={fetchData} value={keyword}>
+        Get Data
+        </button>
+        <table>
+          <tbody>{tableRows}</tbody>
+        </table>
     </div>
   );
 }
